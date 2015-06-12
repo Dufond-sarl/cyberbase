a313a1c16adebf546664464a3d4a4a5ed3b9bda3
sylvainpicard <iris.picard@gmail.com>
2015-06-12 09:57:58 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>CyberBase</title>
+	<style type="text/css" media="screen, print">
+        @font-face {
+          font-family: "Bebas";
+          src: url("Police/BEBAS___.TTF");
+        }
+    </style>
+<link href="styles.css" rel="stylesheet" type="text/css" />
+</head>
+
+    <body>
+    	<main>
+            <header>
+            	
+        
+			<nav>
+                <img class="Logo" src="img/cyberbaselogo.png"/>
+                <ul>
+                	<li>
+                    	<a href="index.php"><img alt="bouton home" src="img/home.png"/></a>
+                    </li>
+                	<li>
+                    	<a href="gestionUtilisateur.php">GESTION DES UTILISATEUR</a>
+                    </li>
+                	<li>
+                    	GESTION DU PARC
+                    </li>
+                	<li>
+                    	<a href="gestionUsager.php">GESTION DES USAGERS</a>
+                    </li>
+                	<li>
+                    	GESTION DES PLANNINGS
+                    </li>
+                	<li>
+                    	STATISTIQUES
+                    </li>
+                </ul>
+                
+             </nav>
+                 </header>
+             <article>
+             
+             	<div class="TextHome">
+                    <h1>Ex his quidam aeternitati.</h1>
+                    <p>
+                        
+                        Ex his quidam aeternitati se commendari posse per statuas aestimantes eas ardenter adfectant quasi plus praemii de figmentis aereis sensu carentibus adepturi, quam ex conscientia honeste recteque factorum, easque auro curant inbracteari,
+                         quod Acilio Glabrioni delatum est primo, cum consiliis armisque regem superasset Antiochum. quam autem sit pulchrum exigua haec spernentem et minima ad ascensus verae gloriae tendere longos et arduos, ut memorat vates Ascraeus, Censorius Cato monstravit. qui interrogatus quam ob rem inter multos... statuam non haberet malo inquit ambigere bonos quam ob rem id non meruerim, quam quod est gravius cur inpetraverim mussitare.
+                    </p>
+                </div>
+                
+                
+                <div class="ImgHome">
+                
+                	<img alt="Image d accueil" src="img/accueil.jpg"/>
+                
+                </div>
+          
+             
+             
+             </article>
+
+		</main>
+	</body>
+</html>
